58cc0d4c727a886c9ba6f2a634177ff3e3111415
AkibMohtasim <mohtasimakib@gmail.com>
2023-05-26 02:21:12 +0600

    accessories new post route added

--- Routes/accessories.js
+++ Routes/accessories.js
@@ -10,6 +10,19 @@ router.get('/', async (req, res) => {
   res.send(accessories);
 })
 
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  const accessorie = await Accessory.findById(id);
+  res.send(accessorie);
+})
+
+router.post('/', async (req, res) => {
+  const accessoryBody = req.body;
+  const newAccessory = new Accessory(accessoryBody);
+  const result = await newAccessory.save();
+  res.send(result);
+})
+
 router.delete('/:id', async (req, res) => {
   const id = req.params.id;
   const deletedAccessory = await Accessory.findByIdAndDelete(id);
